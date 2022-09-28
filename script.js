a4a4be691e791604f27bef0fa07bef1a2ccfb83d
Yilika Loufoua <59208173+YilikaLoufoua@users.noreply.github.com>
2022-09-28 02:12:04 -0500

    complete

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ const apiKey = "34386ec5358ca437ec6fd6b684886f4d";
 const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 
 const moviesGrid = document.getElementById("movies-grid");
+const searchInput = document.getElementById("search-input");
+const searchForm = document.getElementById("search-form");
+const categoryTitle = document.getElementById("category-title");
 
 async function fetchMoviesNowPlaying() {
     const response = await fetch(`${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`);
     const jsonResponse = await response.json();
     const movies = jsonResponse.results;
-    console.log(movies);
+
+    displayMovies(movies);
+}
+
+async function searchMovies(query) {
+    const response = await fetch(`${apiBaseUrl}/search/movie?api_key=${apiKey}&query="${query}"`);
+    const jsonResponse = await response.json();
+    const movies = jsonResponse.results;
 
     displayMovies(movies);
 }
@@ -18,12 +28,20 @@ function displayMovies(movies) {
         .map(
             (movie) => `<div class="movie-card">
                             <img src="${imageBaseUrl}${movie.poster_path}"/>
-                            <p>⭐${movie.vote_average}</p>
+                            <p>⭐ ${movie.vote_average}</p>
                             <h1>${movie.title}</h1>
                         </div>`
         )
         .join("");
 }
 
-fetchMoviesNowPlaying();
+function handleSearchFormSubmit(event) {
+    categoryTitle.innerHTML = "Search Results";
+    event.preventDefault();
+    const searchQuery = searchInput.value;
+    const movies = searchMovies(searchQuery);
+    displayMovies(movies);
+}
 
+searchForm.addEventListener("submit", handleSearchFormSubmit);
+fetchMoviesNowPlaying();
